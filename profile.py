b047cdafd6f24852627492b599404e4ea1bf07e2
Minuarul <117071085+Minuarul@users.noreply.github.com>
2022-10-31 15:16:36 +0530

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,13 +1,37 @@
-import pandas as pd
+"""
+Created on Mon Oct 31 14:54:43 2022
+@author: 1013327
+"""
+
 import streamlit as st
 
-st.title("Hi Anber! Welcome to Streamlit!")
+def intro():
+    import streamlit as st
+    st.write("# Welcome to Streamlit! 👋")
+    st.sidebar.success("Select a demo above.")
+    st.markdown("""Resume Creator""")
 
-st.write("My first DataFrame")
+def mapping_demo():
+    import streamlit as st
+    st.write("# Welcome to Streamlit! 👋")
+    st.sidebar.success("Select a demo above.")
+    
+def plotting_demo():
+    import streamlit as st
+    st.write("# Welcome to Streamlit! 👋")
+    st.sidebar.success("Select a demo above.")
 
-st.write(
-pd.DataFrame({
-    'A': [1, 5, 9, 7],
-    'B': [3, 2, 4, 8]
-  })
-)
+def data_frame_demo():
+    import streamlit as st
+    st.write("# Welcome to Streamlit! 👋")
+    st.sidebar.success("Select a demo above.")
+
+page_names_to_funcs = {
+    "—": intro,
+    "Plotting Demo": plotting_demo,
+    "Mapping Demo": mapping_demo,
+    "DataFrame Demo": data_frame_demo
+}
+
+demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys())
+page_names_to_funcs[demo_name]()
